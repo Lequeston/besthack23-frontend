ddee916018bed53b54f071b8b369a09c2d2e5384
Логачев Кирилл Александрович <kl@level.travel>
2023-04-27 21:42:00 +0300

    Ну что-то есть

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Input, Space, Button, List, Spin, Result } from 'antd';
+import SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
 
-function App() {
+const sock = new SockJS('http://192.168.1.102:8080/exchangeData/');
+
+const client = Stomp.over(sock);
+
+
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [answers, setAnswers] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isServerConnectionFailed, setIsServerConnectionFailed] = useState(false);
+  useEffect(() => {
+    client.connect({}, (frame) => {
+      setIsLoading(false);
+      client.subscribe("/ws/pong", (payload) => {
+        setAnswers((answers) => [...answers, JSON.parse(payload.body).message]);
+      });
+    }, () => {
+      setIsServerConnectionFailed(true);
+    });
+  }, []);
+
+  const handleSend = () => {
+    client.send("/app/ping", {}, JSON.stringify({ message: message }));
+  }
+
+  if (isServerConnectionFailed) {
+    <Result
+      status="error"
+      title="Игорь подыми серв"
+      subTitle="Бля буду работает"
+    />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Spin spinning={isLoading} delay={500}>
+      <Space>
+        <Input placeholder="Basic usage" value={message} onChange={(e) => setMessage(e.target.value)}/>
+        <Button type="primary" onClick={handleSend}>Submit</Button>
+      </Space>
+      <List
+        size="large"
+        bordered
+        dataSource={answers}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+      />
+    </Spin>
   );
 }
 
